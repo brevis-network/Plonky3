b56b40778b1022837c26d6070f2cf7fe4751510f
zktommy <tommy@brevis.network>
2025-04-24 16:21:34 +0800

    fix: add missing const FOUR and FIVE for monty-31 avx2 packing
    Signed-off-by: zktommy <tommy@brevis.network>

--- monty-31/src/x86_64_avx2/packing.rs
+++ monty-31/src/x86_64_avx2/packing.rs
@@ -475,6 +475,8 @@ impl<FP: FieldParameters> FieldAlgebra for PackedMontyField31AVX2<FP> {
     const ZERO: Self = Self::broadcast(MontyField31::ZERO);
     const ONE: Self = Self::broadcast(MontyField31::ONE);
     const TWO: Self = Self::broadcast(MontyField31::TWO);
+    const FOUR: Self = Self::broadcast(MontyField31::FOUR);
+    const FIVE: Self = Self::broadcast(MontyField31::FIVE);
     const NEG_ONE: Self = Self::broadcast(MontyField31::NEG_ONE);
 
     #[inline]
